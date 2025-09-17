af3c1a5876d008bfb02f82a40455880c00387659
Lucas Carvalho <luucas_pg@hotmail.com.br>
2025-09-16 21:05:56 -0300

    fix: Coloca a coluna de email como UNIQUE

--- src/main/java/dev/iluck/CadastroDeNinjas/Models/MissaoModel.java
+++ src/main/java/dev/iluck/CadastroDeNinjas/Models/MissaoModel.java
@@ -1,12 +1,16 @@
 package dev.iluck.CadastroDeNinjas.Models;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissaoModel {
 
     @Id
@@ -19,29 +23,4 @@ public class MissaoModel {
 
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
-
-
-    public MissaoModel() {
-    }
-
-    public MissaoModel(String nome, String dificuldade) {
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
 }
